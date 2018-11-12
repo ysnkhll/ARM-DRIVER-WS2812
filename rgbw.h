64ec89c18f5ba1eb6219bf9e2aca823d696ea701
Yasin Khalil <ysnkhll@users.noreply.github.com>
2018-11-12 11:37:57 -0800

    Update rgbw.h

--- rgbw.h
+++ rgbw.h
@@ -1,5 +1,5 @@
-#ifndef __LAND_RGBW_H__
-#define __LAND_RGBW_H__
+#ifndef __RGBW_H__
+#define __RGBW_H__
 
 #ifdef __cplusplus
 extern "C"
@@ -19,11 +19,6 @@ extern "C"
 */
 #define LED_PULSE_0 0xc0 // 0b11000000
 #define LED_PULSE_1 0xf8 // 0b11111000
-
-#define LED_GREEN   1
-#define LED_ORANGE  2
-#define LED_RED     3
-#define LED_BLUE    4
 
 void LED_Configuration(void);
 void SPI_Configuration(void);
